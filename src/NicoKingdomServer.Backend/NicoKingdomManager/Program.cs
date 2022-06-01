ed9a8d96c35a07d623f8bf944ea57957a3519c5a
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-06-01 16:53:43 +0200

    Program and appsettings.json configuration

--- src/NicoKingdomServer.Backend/NicoKingdomManager/Program.cs
+++ src/NicoKingdomServer.Backend/NicoKingdomManager/Program.cs
@@ -1,25 +1,73 @@
-var builder = WebApplication.CreateBuilder(args);
+using FluentValidation.AspNetCore;
+using Hellang.Middleware.ProblemDetails;
+using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
+using NicoKingdomManager.BusinessLayer.MapperProfiles;
+using NicoKingdomManager.BusinessLayer.Services;
+using NicoKingdomManager.BusinessLayer.Services.Common;
+using NicoKingdomManager.BusinessLayer.Validators;
+using NicoKingdomManager.DataAccessLayer;
+using Serilog;
+using System.Text.Json.Serialization;
+using TinyHelpers.Json.Serialization;
 
-// Add services to the container.
+namespace NicoKingdomManager;
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static async Task Main(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        ConfigureSerilog(builder.Host);
+        ConfigureServices(builder.Services, builder.Configuration);
+
+        WebApplication app = builder.Build();
+        Configure(app);
+        await app.RunAsync();
+    }
+    private static void ConfigureSerilog(IHostBuilder host)
+    {
+        host.UseSerilog((hostingContext, loggerConfiguration) =>
+        {
+            loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration);
+        });
+    }
+    private static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddProblemDetails();
+        services.AddControllers()
+        .AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
+            options.JsonSerializerOptions.Converters.Add(new UtcDateTimeConverter());
+        });
+        services.AddEndpointsApiExplorer();
+        services.AddAutoMapper(typeof(UserMapperProfile).Assembly);
+        services.AddFluentValidation(options =>
+        {
+            options.RegisterValidatorsFromAssemblyContaining<SaveUserValidator>();
+        });
+        services.AddSwaggerGen()
+        .AddFluentValidationRulesToSwagger(options =>
+        {
+            options.SetNotNullableIfMinLengthGreaterThenZero = true;
+        });
+
+        string connectionString = configuration.GetConnectionString("SqlConnection");
+        services.AddSqlServer<DataContext>(connectionString);
+        services.AddScoped<IDataContext>(sp => sp.GetRequiredService<DataContext>());
+
+        services.AddScoped<IUsersService, UsersService>();
+    }
+    private static void Configure(IApplicationBuilder app)
+    {
+        app.UseProblemDetails();
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.UseRouting();
+        app.UseHttpsRedirection();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
