a3788749b35e0ba2c27b67bb1dfd2e1fffe9a8f9
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-06-02 22:34:22 +0200

    added placeholder class that inherits from ControllerBase to avoid code duplication of the attributes

---
+++ src/NicoKingdomServer.Backend/NicoKingdomManager/Controllers/Common/ApiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NicoKingdomManager.Controllers.Common;
+
+/// <summary>
+/// this controller is used only as placeholder to avoid code duplication of the
+/// 3 attributes below
+/// </summary>
+[ApiController]
+[Route("api/[controller]")]
+[Route("application/json")]
+public abstract class ApiController : ControllerBase
+{
+    protected ApiController() : base()
+    {
+    }
+}
