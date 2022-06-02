4799b4770cb4ad96e2546b2948043625595a288f
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-06-02 18:06:05 +0200

    added Startup.cs file and code re-organization

--- src/NicoKingdomServer.Backend/NicoKingdomManager/Program.cs
+++ src/NicoKingdomServer.Backend/NicoKingdomManager/Program.cs
@@ -1,74 +1,16 @@
-using FluentValidation.AspNetCore;
-using Hellang.Middleware.ProblemDetails;
-using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
-using NicoKingdomManager.BusinessLayer.MapperProfiles;
-using NicoKingdomManager.BusinessLayer.Services;
-using NicoKingdomManager.BusinessLayer.Services.Common;
-using NicoKingdomManager.BusinessLayer.Validators;
-using NicoKingdomManager.DataAccessLayer;
-using Serilog;
-using System.Text.Json.Serialization;
-using TinyHelpers.Json.Serialization;
-
 namespace NicoKingdomManager;
 
 public class Program
 {
     public static async Task Main(string[] args)
     {
-        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-        ConfigureSerilog(builder.Host);
-        ConfigureServices(builder.Services, builder.Configuration);
+        WebApplicationBuilder builder = CreateBuilder(args);
+        Startup startup = new();
+        startup.ConfigureServices(builder.Services, builder.Configuration, builder.Host);
 
         WebApplication app = builder.Build();
-        Configure(app);
+        startup.Configure(app);
         await app.RunAsync();
     }
-    private static void ConfigureSerilog(IHostBuilder host)
-    {
-        host.UseSerilog((hostingContext, loggerConfiguration) =>
-        {
-            loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration);
-        });
-    }
-    private static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
-    {
-        services.AddProblemDetails();
-        services.AddControllers()
-        .AddJsonOptions(options =>
-        {
-            options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
-            options.JsonSerializerOptions.Converters.Add(new UtcDateTimeConverter());
-        });
-        services.AddEndpointsApiExplorer();
-        services.AddAutoMapper(typeof(UserMapperProfile).Assembly);
-        services.AddFluentValidation(options =>
-        {
-            options.RegisterValidatorsFromAssemblyContaining<SaveUserValidator>();
-        });
-        services.AddSwaggerGen()
-        .AddFluentValidationRulesToSwagger(options =>
-        {
-            options.SetNotNullableIfMinLengthGreaterThenZero = true;
-        });
-
-        string connectionString = configuration.GetConnectionString("SqlConnection");
-        services.AddSqlServer<DataContext>(connectionString);
-        services.AddScoped<IDataContext>(sp => sp.GetRequiredService<DataContext>());
-
-        services.AddScoped<IUsersService, UsersService>();
-        services.AddScoped<IRolesService, RolesService>();
-    }
-    private static void Configure(IApplicationBuilder app)
-    {
-        app.UseProblemDetails();
-        app.UseSwagger();
-        app.UseSwaggerUI();
-        app.UseRouting();
-        app.UseHttpsRedirection();
-        app.UseEndpoints(endpoints =>
-        {
-            endpoints.MapControllers();
-        });
-    }
+    private static WebApplicationBuilder CreateBuilder(string[] args) => WebApplication.CreateBuilder(args);
 }
---
+++ src/NicoKingdomServer.Backend/NicoKingdomManager/Startup.cs
@@ -0,0 +1,66 @@
+﻿using FluentValidation.AspNetCore;
+using Hellang.Middleware.ProblemDetails;
+using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
+using NicoKingdomManager.BusinessLayer.MapperProfiles;
+using NicoKingdomManager.BusinessLayer.Services;
+using NicoKingdomManager.BusinessLayer.Services.Common;
+using NicoKingdomManager.BusinessLayer.Validators;
+using NicoKingdomManager.DataAccessLayer;
+using Serilog;
+using System.Text.Json.Serialization;
+using TinyHelpers.Json.Serialization;
+
+namespace NicoKingdomManager;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services, IConfiguration configuration, IHostBuilder host)
+    {
+        host.UseSerilog((hostingContext, loggerConfiguration) =>
+        {
+            loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration);
+        });
+
+        services.AddProblemDetails();
+        services.AddControllers()
+        .AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
+            options.JsonSerializerOptions.Converters.Add(new UtcDateTimeConverter());
+        });
+        services.AddEndpointsApiExplorer();
+        services.AddAutoMapper(typeof(UserMapperProfile).Assembly);
+        services.AddFluentValidation(options =>
+        {
+            options.RegisterValidatorsFromAssemblyContaining<SaveUserValidator>();
+        });
+        services.AddSwaggerGen()
+        .AddFluentValidationRulesToSwagger(options =>
+        {
+            options.SetNotNullableIfMinLengthGreaterThenZero = true;
+        });
+
+        string connectionString = configuration.GetConnectionString("SqlConnection");
+        services.AddSqlServer<DataContext>(connectionString);
+        services.AddScoped<IDataContext>(sp => sp.GetRequiredService<DataContext>());
+
+        services.AddScoped<IUsersService, UsersService>();
+        services.AddScoped<IRolesService, RolesService>();
+    }
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseProblemDetails();
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.UseSerilogRequestLogging(options =>
+        {
+            options.IncludeQueryInRequestPath = true;
+        });
+        app.UseRouting();
+        app.UseHttpsRedirection();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
