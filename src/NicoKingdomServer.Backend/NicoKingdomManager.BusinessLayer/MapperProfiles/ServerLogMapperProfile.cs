b2f0fcf662b2660acba3f6a68052cd81d0e17112
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-06-02 22:08:10 +0200

    added validators and mapper profile for ServerLog model and SaveLogRequest model

---
+++ src/NicoKingdomServer.Backend/NicoKingdomManager.BusinessLayer/MapperProfiles/ServerLogMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using NicoKingdomManager.Shared.Models;
+using NicoKingdomManager.Shared.Models.Requests;
+using Entities = NicoKingdomManager.DataAccessLayer.Entities;
+
+namespace NicoKingdomManager.BusinessLayer.MapperProfiles;
+
+public class ServerLogMapperProfile : Profile
+{
+    public ServerLogMapperProfile()
+    {
+        CreateMap<Entities.ServerLog, ServerLog>();
+        CreateMap<SaveLogRequest, Entities.ServerLog>();
+    }
+}
---
+++ src/NicoKingdomServer.Backend/NicoKingdomManager.BusinessLayer/Validators/SaveLogValidator.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation;
+using NicoKingdomManager.Shared.Models.Requests;
+
+namespace NicoKingdomManager.BusinessLayer.Validators;
+
+public class SaveLogValidator : AbstractValidator<SaveLogRequest>
+{
+    public SaveLogValidator()
+    {
+        RuleFor(log => log.UserName)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("you must enter your username");
+
+        RuleFor(log => log.Title)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("you must enter the title");
+
+        RuleFor(log => log.Action)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("you must enter the action");
+
+        RuleFor(log => log.Description)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("you must enter the description");
+
+        RuleFor(log => log.UserName)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("you must enter your username");
+
+        RuleFor(log => log.LogDate)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("you must enter the date of the log");
+
+        RuleFor(log => log.LogType)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("you must add the type of the log");
+    }
+}
