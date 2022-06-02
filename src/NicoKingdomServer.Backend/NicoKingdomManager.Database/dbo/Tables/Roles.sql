bd0e3b07404145eb590b5766444554c6b1e4182a
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-06-02 12:43:24 +0200

    updated database

--- src/NicoKingdomServer.Backend/NicoKingdomManager.Database/dbo/Tables/Roles.sql
+++ src/NicoKingdomServer.Backend/NicoKingdomManager.Database/dbo/Tables/Roles.sql
@@ -2,8 +2,8 @@
     [Id]               UNIQUEIDENTIFIER CONSTRAINT [DF_Roles_Id] DEFAULT (newid()) NOT NULL,
     [Name]             NVARCHAR (100)   NOT NULL,
     [Color]            NVARCHAR (10)    NOT NULL,
-    [CreatedDate]      DATETIME         NOT NULL,
-    [LastModifiedDate] DATETIME         NULL,
+    [CreatedDate]      DATE         NOT NULL,
+    [LastModifiedDate] DATE         NULL,
     CONSTRAINT [PK_Roles] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
--- src/NicoKingdomServer.Backend/NicoKingdomManager.Database/dbo/Tables/Users.sql
+++ src/NicoKingdomServer.Backend/NicoKingdomManager.Database/dbo/Tables/Users.sql
@@ -2,8 +2,8 @@
     [Id]               UNIQUEIDENTIFIER CONSTRAINT [DF_Users_Id] DEFAULT (newid()) NOT NULL,
     [UserName]         NVARCHAR (100)   NOT NULL,
     [NickName]         NVARCHAR (100)   NOT NULL,
-    [CreatedDate]      DATETIME         NOT NULL,
-    [LastModifiedDate] DATETIME         NULL,
+    [CreatedDate]      DATE         NOT NULL,
+    [LastModifiedDate] DATE         NULL,
     CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
