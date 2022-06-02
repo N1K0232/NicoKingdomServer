afdeb4c19e9adc1b6a082eb8fb3a3087b766f345
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-06-02 22:00:27 +0200

    changed property name from User to UserName to avoid problems with Automapper package during code execution

--- src/NicoKingdomServer.Backend/NicoKingdomManager.Shared/Models/Requests/SaveLogRequest.cs
+++ src/NicoKingdomServer.Backend/NicoKingdomManager.Shared/Models/Requests/SaveLogRequest.cs
@@ -11,7 +11,7 @@ namespace NicoKingdomManager.Shared.Models.Requests;
 
 public class SaveLogRequest : BaseRequestObject
 {
-    public string User { get; set; } = "";
+    public string UserName { get; set; } = "";
     public string Title { get; set; } = "";
     public string Action { get; set; } = "";
     public string Description { get; set; } = "";
